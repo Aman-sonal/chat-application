964d82994312c985fcc75312895f4f8e9ad04204
Aman Sonal <amansonal.kit@gmail.com>
2021-04-24 04:33:51 +0530

    inital commit

---
+++ public/client.js
@@ -0,0 +1,39 @@
+const socket= io(); // io is coming from client library
+
+let name1;
+let messageArea;document.querySelector('.message_area');
+let textarea= document.querySelector('#textarea');
+do{
+    name1 = prompt('Enter yourt name');
+}while(!name1);
+
+textarea.addEventListener('keyup', (e) =>{
+    if(e.key=== 'Enter')
+    {
+        sendMessage(e.target.value);
+    }
+})
+function sendMessage(msg1)
+{
+    let msg= {
+        user:name1,
+        message: msg1.trim()
+    }
+    // msg append
+    appendMessage(msg, 'outgoing');
+
+    //send to server
+    socket.emit('message', msg);
+}
+function appendMessage(msg, type)
+{
+    let mainDiv= document.createElement('div');
+    let className= type;
+    mainDiv.classList.add(className,'message');
+    let markup= `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML= markup;
+    messageArea.appendChild(mainDiv);
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+const express= require('express');
+const app= express();
+const http= require('http').createServer(app);
+
+const PORT=process.env.PORT || 3100;
+
+http.listen(PORT, () =>{
+    console.log(`listening on Port ${PORT}`);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname+ '/index.html');
+});
+
+
+//sockets setup
+const io= require('socket.io')(http);
+
+io.on('connection' ,(sock) =>{ // gets called whenever a client is connected to a browser.
+    console.log('Connected...');
+    sock.on('message', (msg) =>{
+        console.log("hello");
+    })
+})
